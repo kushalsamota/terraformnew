0cef32fe5d9e47e63fc9c32fc6f6b2952fe0a5f2
kushal <kushalsamota@gmail.com>
2026-09-05 15:46:47 +0530

    Added backend configuration

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,12 @@ resource "aws_instance" "web" {
     instance_type = "t2.micro"
     ami = "ami-090d68841c2a28756"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraformaugust-12345"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    use_lockfile = true
+  }
+}
